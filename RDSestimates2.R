4a879de8872c95e2cdd2dd69ab561f28fe7a1f11
AlexMRuch <amr442@cornell.edu>
2018-04-24 20:53:25 -0400

    first commit

---
+++ RDSestimates2.R
@@ -0,0 +1,16 @@
+# Soc 6350: Final Project
+# Authors: Alec McGail and Alexander Ruch
+# Last updated: 4/24/2018
+
+# Dependencies: igraph, network, sna, gplots, statnet
+
+################################################################################
+################################################################################
+# Set working directory
+setwd("/Users/amruch/Documents/Research/~ Projects/Cornell Research/RDS-simulation")
+
+################################################################################
+# Run simulations with NetSim
+
+################################################################################
+# Run simulations with EpiModel
